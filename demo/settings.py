89a876fa2a7db3996efeaa2ceb6370792fa25f5c
James  Hovious <hoviousj@gmail.com>
2014-03-20 01:47:48 +0100

    adding procfile

--- demo/settings.py
+++ demo/settings.py
@@ -19,14 +19,14 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'demo.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'ddsaaq8oqpev28',
+    'HOST': 'ec2-23-23-244-144.compute-1.amazonaws.com',
+    'PORT': 5432,
+    'USER': 'hwtozdbvvlusfx',
+    'PASSWORD': '0ZSbikGugaibALGwuUQORWqR8J'
+  }
 }
 
 TIME_ZONE = 'America/Chicago'
